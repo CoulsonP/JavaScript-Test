88ba4b5ca5fa0aedc81864ad8de6bebbf57a19f3
CoulsonP <coulsonp@icloud.com>
2022-11-07 14:15:49 -0600

    added notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,40 +9,37 @@ var quest3 = document.querySelector ("#quest3")
 var quest4 = document.querySelector ("#quest4")
 var quest5 = document.querySelector ("#quest5")
 var last = document.querySelector ("#last")
-var your_score = document.querySelector ("#your_score")
+var yourScore = document.querySelector ("#your_score")
 var scores = document.querySelector (".scores")
 var initials = document.querySelector ("#initials")
 var submit = document.querySelector ("#submit")
 var timerInterval;
-
+// countdown timer
 function countdown() {
     timeLeft --
     time.textContent = timeLeft
 
     if (timeLeft <= 0) {
-        console.log("gameover")
         clearInterval(timerInterval)
     }
 }
-
+// starts timer and quiz
 function quizStart() {
     timerInterval = setInterval(countdown, 1000)
     begin.setAttribute("class", "hide")
     quest1.removeAttribute("class", "hide")
 }
-
+// selectors for which answer is chosen
 document.querySelector ("#questbtn1").onclick = checkQuest1
 document.querySelector ("#questbtn2").onclick = checkQuest1
 document.querySelector ("#questbtn3").onclick = checkQuest1
 document.querySelector ("#questbtn4").onclick = checkQuest1
-
+// return correct if correct answer is chose if not send false
 function checkQuest1() {
     if(this.value === "true") {
-        console.log ("correct")
         alert("correct")
     }
     else {
-        console.log ("incorrect")
         alert("incorrect")
         timeLeft = timeLeft -15
         time.textContent = timeLeft
@@ -55,14 +52,12 @@ document.querySelector ("#twoquestbtn1").onclick = checkQuest2
 document.querySelector ("#twoquestbtn2").onclick = checkQuest2
 document.querySelector ("#twoquestbtn3").onclick = checkQuest2
 document.querySelector ("#twoquestbtn4").onclick = checkQuest2
-
+// return correct if correct answer is chose if not send false
 function checkQuest2() {
     if(this.value === "true") {
-        console.log ("correct")
         alert("correct")
     }
     else {
-        console.log ("incorrect")
         alert("incorrect")
         timeLeft = timeLeft -15
         time.textContent = timeLeft
@@ -75,14 +70,12 @@ document.querySelector ("#threequestbtn1").onclick = checkQuest3
 document.querySelector ("#threequestbtn2").onclick = checkQuest3
 document.querySelector ("#threequestbtn3").onclick = checkQuest3
 document.querySelector ("#threequestbtn4").onclick = checkQuest3
-
+// return correct if correct answer is chose if not send false
 function checkQuest3() {
     if(this.value === "true") {
-        console.log ("correct")
         alert("correct")
     }
     else {
-        console.log ("incorrect")
         alert("incorrect")
         timeLeft = timeLeft -15
         time.textContent = timeLeft
@@ -95,14 +88,12 @@ document.querySelector ("#fourquestbtn1").onclick = checkQuest4
 document.querySelector ("#fourquestbtn2").onclick = checkQuest4
 document.querySelector ("#fourquestbtn3").onclick = checkQuest4
 document.querySelector ("#fourquestbtn4").onclick = checkQuest4
-
+// return correct if correct answer is chose if not send false
 function checkQuest4() {
     if(this.value === "true") {
-        console.log ("correct")
         alert("correct")
     }
     else {
-        console.log ("incorrect")
         alert("incorrect")
         timeLeft = timeLeft -15
         time.textContent = timeLeft
@@ -115,19 +106,18 @@ document.querySelector ("#fivequestbtn1").onclick = checkQuest5
 document.querySelector ("#fivequestbtn2").onclick = checkQuest5
 document.querySelector ("#fivequestbtn3").onclick = checkQuest5
 document.querySelector ("#fivequestbtn4").onclick = checkQuest5
-
+// return correct if correct answer is chose if not send false
 function checkQuest5() {
     if(this.value === "true") {
-        console.log ("correct")
         alert("correct")
     }
     else {
-        console.log ("incorrect")
         alert("incorrect")
         timeLeft = timeLeft -15
         time.textContent = timeLeft
     }
     quest5.setAttribute("class", "hide")
+    yourScore.setAttribute("class", "hide")
 }
 
 
@@ -140,5 +130,5 @@ function checkQuest5() {
 
 
 
-// at end of check quest 5 set attr hide on quest 5div remove attr hide on last var
+
 startbtn.onclick = quizStart
